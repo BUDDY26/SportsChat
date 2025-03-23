c6b81ceaf4265bdd3ec3e771c214d03140d7234b
Ruben James Aleman <ruben.aleman@gmail.com>
2025-03-23 12:25:47 -0500

    Fix database connectivity with correct column names and add test page

--- sportschat-frontend/src/App.js
+++ sportschat-frontend/src/App.js
@@ -5,6 +5,7 @@ import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import HomePage from './pages/HomePage';
 import ForgotPasswordPage from './pages/ForgotPasswordPage';
+import SimplifiedDatabaseTestPage from './pages/SimplifiedDatabaseTestPage';
 import './pages/style.css';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+      <Route path="/test-database" element={<SimplifiedDatabaseTestPage />} />
     </Routes>
   );
 }
